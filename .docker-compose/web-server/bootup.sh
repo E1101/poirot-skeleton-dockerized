c374611cf21fb4fb57ecd16b56d7c992d44384b0
Payam Naderi <naderi.payam@gmail.com>
2017-05-13 13:07:01 +0430

    enable cli poirot commands; fix xdebug installation

--- .docker-compose/web-server/bootup.sh
+++ .docker-compose/web-server/bootup.sh
@@ -10,10 +10,15 @@ if [[ "${DEBUG:?}" == "true" ]]; then
     printf "\033[0;32m > Installing XDebug ...\n"
 
     ## install xdebug
-    apt-get install -yq --force-yes --fix-missing php-xdebug
-    echo "xdebug.remote_enable=1" >> /etc/php/7.0/apache2/conf.d/20-xdebug.ini
-    echo "xdebug.remote_host=${HOST_IP}" >> /etc/php/7.0/apache2/conf.d/20-xdebug.ini
-    echo "xdebug.profiler_enable=0" >> /etc/php/7.0/apache2/conf.d/20-xdebug.ini
+    # old
+    #PHPCONF="/etc/php/7.0/apache2/conf.d/20-xdebug.ini"
+    #apt-get install -yq --force-yes --fix-missing php-xdebug
+
+    apt-get install -yq --force-yes --fix-missing php5-xdebug
+    PHPCONF="/usr/local/etc/php/200-xdebug.ini"
+    echo "xdebug.remote_enable=1" >> ${PHPCONF}
+    echo "xdebug.remote_host=${HOST_IP}" >> ${PHPCONF}
+    echo "xdebug.profiler_enable=0" >> ${PHPCONF}
 
     ## install composer
     printf "\033[0;32m > Installing Composer Packages ...\n"
